d01725cf7dbdf06325a4e812b8c1a550db192118
Wang Hui <hui.wang@funplus.com>
2019-11-04 20:57:22 +0800

    library/math add MaxInt32 for script and auto go fmt

--- library/math/library.go
+++ library/math/library.go
@@ -1,59 +1,65 @@
 package math
 
 import (
-    "math/rand"
-    "strconv"
-    "time"
+	"math"
+	"math/rand"
+	"strconv"
+	"time"
 
-    "tklibs/script"
-    "tklibs/script/runtime/function/native"
+	"tklibs/script"
+	"tklibs/script/runtime/function/native"
 )
 
 type library struct {
-    context interface{}
-    ToInt,
-    Rand native.FunctionType
+	context interface{}
+	ToInt,
+	MaxInt32,
+	Rand native.FunctionType
 }
 
 func (*library) GetName() string {
-    return "math"
+	return "math"
 }
 
 func (l *library) SetScriptContext(context interface{}) {
-    l.context = context
+	l.context = context
 }
 
 func NewLibrary() *library {
-    ret := &library{}
-    ret.init()
-    return ret
+	ret := &library{}
+	ret.init()
+	return ret
 }
 
 func (l *library) init() {
-    l.ToInt = func(this interface{}, args ...interface{}) interface{} {
-        if len(args) < 1 {
-            return script.Null
-        }
+	l.ToInt = func(this interface{}, args ...interface{}) interface{} {
+		if len(args) < 1 {
+			return script.Null
+		}
 
-        switch v := args[0].(type) {
-        case script.Int:
-            return v
-        case script.Float:
-            return script.Int(v)
-        case script.Bool:
-            if v {
-                return 1
-            }
-            return 0
-        case script.String:
-            val, _ := strconv.Atoi(string(v))
-            return script.Int(val)
-        default:
-            return 0
-        }
-    }
+		switch v := args[0].(type) {
+		case script.Int:
+			return v
+		case script.Float:
+			return script.Int(v)
+		case script.Bool:
+			if v {
+				return 1
+			}
+			return 0
+		case script.String:
+			val, _ := strconv.Atoi(string(v))
+			return script.Int(val)
+		default:
+			return 0
+		}
+	}
 
-    l.Rand = func(this interface{}, args ...interface{}) interface{} {
-        return script.Float(rand.New(rand.NewSource(time.Now().UnixNano())).Float32())
-    }
+	l.Rand = func(this interface{}, args ...interface{}) interface{} {
+		return script.Float(rand.New(rand.NewSource(time.Now().UnixNano())).Float32())
+	}
+
+	l.MaxInt32 = func(this interface{}, args ...interface{}) interface{} {
+		return math.MaxInt32
+	}
 }
