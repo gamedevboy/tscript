02dcac1084e05c5cb9eeaf2317d41bf7bfbbdbeb
Wang Hui <hui.wang@funplus.com>
2019-11-05 13:08:32 +0800

    assembly/loader add LoadAssemblySourceFromBuffer mainly for testing

--- assembly/loader/loader.go
+++ assembly/loader/loader.go
@@ -3,11 +3,38 @@ package loader
 import (
     "bufio"
     "bytes"
+    "io"
     "os"
+    "unsafe"
 
     "tklibs/script"
     "tklibs/script/compiler/compiler"
 )
+
+//LoadAssemblySourceFromBuffer just load 'source file' from reader mainly for testing
+func LoadAssemblySourceFromBuffer(assembly interface{},reader ...io.Reader) error {
+    scriptCompiler := &struct {
+        *compiler.Component
+    }{}
+    scriptCompiler.Component = compiler.NewCompiler(scriptCompiler)
+
+    buf := new(bytes.Buffer)
+    for _, r := range reader {
+        buf.Reset()
+        if _, err :=buf.ReadFrom(r) ; err != nil {
+            return err
+        }
+        b := buf.Bytes()
+        scriptCompiler.AddSource(*(*string)(unsafe.Pointer(&b)))
+    }
+
+    asm, _, _ := scriptCompiler.Compile()
+    buffer := &bytes.Buffer{}
+    asm.(script.Assembly).Save(bufio.NewWriter(buffer))
+    assembly.(script.Assembly).Load(bufio.NewReader(buffer))
+    return nil
+}
+
 
 func LoadAssembly(assembly interface{}, files ...string) error {
     buffer := &bytes.Buffer{}
